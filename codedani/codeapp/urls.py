5f00ad4a8a02ee9339b881c8ac951457661f122f
Danish khan <danishyazdani063@gmail.com>
2024-09-18 19:48:32 +0500

    update

--- codedani/codeapp/urls.py
+++ codedani/codeapp/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path("", views.homepage, name="homepage"),
     # path('ali/', views.ali, name='ali'),
     path('about/',views.about,name='about'),
+    path('blog/',views.blog,name='blog'),
+    path('contact/',views.contact,name='contact'),
     path('ali/<slug:code>/', views.alidetails, name='alidetails'),
 
     
