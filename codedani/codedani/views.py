dbbfb0d9acac2c43af781ba7b621a46a49c6950a
Danish khan <danishyazdani063@gmail.com>
2024-09-18 10:39:37 +0500

    first commit

---
+++ codedani/codedani/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def homepage(request):
+    data={
+        'title':'home ',
+         'alpha':'software engineering'
+    }
+    return render(request, "index.html", data)
+ 
+def dani(request):
+    return HttpResponse("welcome again dani")
+
+
+def ali(reques):
+    return HttpResponse("welcom ali")
+
+
+def alidetails(request, code):
+    return HttpResponse(code)
